4c533a51c3fabaaa8242b235fdc12e8a30cc9db9
Denisse Z <unrealistc@gmail.com>
2019-10-16 18:42:14 -0700

    Tarea Stack Queue

---
+++ Recursion/StackQueue.py
@@ -0,0 +1,64 @@
+class Node:
+    def __init__(self, num):
+        self.num = num
+        self.next = None
+
+
+class Stack:
+    def __init__(self):
+        self.root = None
+
+    def push(self, num):
+        if self.root is None:
+            self.root = Node(num)
+        else:
+            cur = self.root
+            while cur.next is not None:
+                cur = cur.next
+            cur.next = Node(num)
+
+    # Quitar el primer elemento de la lista y regresar su valor
+    def pop(self):
+        cur = self.root
+        print (cur.num)
+        self.root = cur.next
+        # num del elemento que quitaron
+
+    def print_nodes(self):
+        cur = self.root
+
+        while cur is not None:
+            p += str(cur.num) + ", "
+            cur = cur.next
+        print(p[:-2])
+
+
+class Queue:
+    def __init__(self):
+        self.root = None
+
+    def push(self, num):
+        if self.root is None:
+            self.root = Node(num)
+        else:
+            cur = self.root
+            while cur.next is not None:
+                cur = cur.next
+            cur.next = Node(num)
+
+    # Quitar el último elemento de la lista y regresar su valor
+    def pop(self):
+        self.root = cur.next.next
+        print (cur.next.next.num)
+
+
+ # num del elemento que quitaron
+
+    def print_nodes(self):
+        cur = self.root
+        p = ""
+        while cur is not None:
+            p += str(cur.num) + ", "
+            cur = cur.next
+        print(p[:-2])
+

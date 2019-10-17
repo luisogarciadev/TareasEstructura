359cf68df922b9f82c3718b96c9661807bafc9cd
José A. Aceves Covarrubias <jose.aceves@cetys.edu.mx>
2019-10-16 18:22:11 -0700

    Tarea Stack Queue

---
+++ StackQueue/07-StackQueue - 27160.py
@@ -0,0 +1,61 @@
+class Node:
+    def __init__(self, num):
+        self.num = num
+        self.next = None
+
+
+class Stack:
+    def __init__(self):
+        self.root = None
+
+    def push(self, num):
+        if self.root is None:
+            self.root = Node(num)
+        else:
+            cur = self.root
+            while cur.next is not None:
+                cur = cur.next
+            cur.next = Node(num)
+
+    # Quitar el primer elemento de la lista y regresar su valor
+    def pop(self):
+        cur = self.root
+        while cur.next is not None:
+            cur = cur.next
+        cur = None
+        return  # num del elemento que quitaron
+
+    def print_nodes(self):
+        cur = self.root
+        p = ""
+        while cur is not None:
+            p += str(cur.num) + ", "
+            cur = cur.next
+        print(p[:-2])
+
+
+class Queue:
+    def __init__(self):
+        self.root = None
+
+    def push(self, num):
+        if self.root is None:
+            self.root = Node(num)
+        else:
+            cur = self.root
+            while cur.next is not None:
+                cur = cur.next
+            cur.next = Node(num)
+
+    # Quitar el último elemento de la lista y regresar su valor
+    def pop(self):
+        self.root = cur.next
+        return  # num del elemento que quitaron
+
+    def print_nodes(self):
+        cur = self.root
+        p = ""
+        while cur is not None:
+            p += str(cur.num) + ", "
+            cur = cur.next
+        print(p[:-2])
